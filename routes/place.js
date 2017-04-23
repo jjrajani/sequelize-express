dc4639338aecb029079222b6f3c059d89d320746
humdrum <jjrajani@gmail.com>
2017-04-22 20:44:52 -0400

    private functions.

--- routes/place.js
+++ routes/place.js
@@ -7,38 +7,28 @@ router.get('/:place_id', function(req, res) {
   models.Place.findOne({ where: {id: req.params.place_id},
     include: [ models.Link ]
   }).then(function(place) {
-    renderPlace(res, place);
+    _renderPlace(res, place);
   });
 });
 
 /* POST update place. */
 router.post('/:place_id/update', function(req, res) {
-  let update = validateUpdate(req.body);
+  let update = _validateUpdate(req.body);
   models.Place.update(update, { where: {id: req.params.place_id}
   }).then(function(place) {
-    console.log("===================")
-    console.log("===================")
-    console.log("===================")
-    console.log("after update", place)
-    console.log("===================")
-    console.log("===================")
-    console.log("===================")
-    // renderPlace(res, place);
     res.redirect('/place/' + place);
   });
 });
 
-
-
-
-var renderPlace = function(res, place) {
+/* Private Functions */
+var _renderPlace = function(res, place) {
   res.render('place', {
     title: "humdrum's List of Places",
     place: place
   });
 }
 
-var validateUpdate = function(body) {
+var _validateUpdate = function(body) {
   let update = {};
   Object.keys(body).forEach((key) => {
     if (body[key] !== "") {
